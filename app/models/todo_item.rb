badcb31befd4535c3b13fb32d1c1ec313747cd17
yatharth saxena <y.s.saxena7@gmail.com>
2020-05-21 09:20:26 +0530

    Pushing 8th time

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -3,4 +3,6 @@ class TodoItem < ApplicationRecord
 def completed?
    !completed_at.blank?
   end
+   
+  default_scope { order(priority: :desc , due_date: :desc) }
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,5 @@
 class TodoList < ApplicationRecord
 	has_many :todo_items
+
+	default_scope { order(created_at: :desc) }
 end
